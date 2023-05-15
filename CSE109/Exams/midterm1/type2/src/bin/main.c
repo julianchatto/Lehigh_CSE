8fbf963947be870a88726492b61f6510bb0244ef
Julian Chattopadhyay <juc226@lehigh.edu>
2023-05-15 13:02:17 -0400

    Delete CSE109 directory

--- CSE109/Exams/midterm1/type2/src/bin/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include "function.h"
-#include <stdio.h>
-
-int main(int argc, char **argv) {
-    printf("%d", function1(2, 3));
-    return 0;
-}
--- CSE109/Exams/midterm1/type2/src/function.c
+++
@@ -1,4 +0,0 @@
-#include <stdlib.h>
-int function1(int x, int y) {
-    return x + y; 
-}
